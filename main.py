ddc02513cb50aa24ddc7a45ce3f60d4ba3605154
Kumar <kumaraswamythale@gmail.com>
2026-05-07 20:52:11 +0530

    Initial EMI Calculator setup

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 print("Project Started")
+#heading
+print("==================")
+print("EMI CALCULATOR")
+print("==================")
+
+#gathering inputs
+customer_name = input("Enter Customer Name:")
+loan_amount = float(input("Enter the Loan Amount"))
+interest_rate = float(input("Enter the Interest Rate"))
+
